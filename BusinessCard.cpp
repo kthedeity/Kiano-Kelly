52a24ed077c84d63b3d3cc855aeed9cf91d7f378
kthedeity <88444617+kthedeity@users.noreply.github.com>
2023-02-15 09:23:06 -0600

    Add files via upload

---
+++ BusinessCard.cpp
@@ -0,0 +1,21 @@
+//Kiano Kelly
+//09-09-20
+/*Print out Business card */
+
+//Library Package
+#include <iostream>
+using namespace std;
+
+//main function
+int main() //header
+{ //beginning of the function
+ cout <<"*******************" << endl << endl;
+ cout << "Kiano Kelly" << endl << endl ;
+ //endl - end of the line go to a new line
+ cout << "123 Sesame Street" << endl << endl;
+ cout << "Irving, Tx 75063" << endl << endl;
+ cout << "910-992-2388" << endl << endl; 
+ cout << "kdkelly07@gmail.com" << endl << endl;
+ cout <<"*******************" << endl << endl;
+return 0;
+} //ending of the function 
