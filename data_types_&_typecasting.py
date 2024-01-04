75c6088ee193d90fb15208d163310e53300f5fb7
kthedeity <88444617+kthedeity@users.noreply.github.com>
2024-01-04 14:45:40 -0600

    Create data_types_&_typecasting.py

---
+++ data_types_&_typecasting.py
@@ -0,0 +1,13 @@
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+
+#typecasting
+
+new_2digitnum = str(two_digit_number)
+x1 = int(new_2digitnum[0])
+x2 = int(new_2digitnum[1])
+
+print(x1 + x2)
+
