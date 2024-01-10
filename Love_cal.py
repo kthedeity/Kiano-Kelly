379a00416bdd546adec0eca4838b73123e657d07
kthedeity <88444617+kthedeity@users.noreply.github.com>
2024-01-09 21:07:44 -0600

    Update Love_cal.py

--- Love_cal.py
+++ Love_cal.py
@@ -1,34 +1,22 @@
 #Love cal
 print("The Love Calculator is calculating your score...")
 name1 = input() # What is your name?
-name2 = input() # what is your middle name
-name3 = input() # What is their name?
-
+name2 = input() # what is your name?
 
 combine_name = name1 + " " + name2
 lower_txt = combine_name.lower()
 
-substringt = "t"
-substringr = "r"
-substringu = "u"
-substringe = "e"
-substringl = "l"
-substringo = "o"
-substringv = "v"
-substringe = "e"
-count_t = lower_txt.count(substringt)
-count_r = lower_txt.count(substringr)
-count_u = lower_txt.count(substringu)
-count_e = lower_txt.count(substringe)
-count_l = lower_txt.count(substringl)
-count_o= lower_txt.count(substringo)
-count_v = lower_txt.count(substringv)
-count_e = lower_txt.count(substringe)
+count_t = lower_txt.count("t")
+count_r = lower_txt.count("r")
+count_u = lower_txt.count("u")
+count_e = lower_txt.count("e")
+count_l = lower_txt.count("l")
+count_o= lower_txt.count("o")
+count_v = lower_txt.count("v")
+count_e = lower_txt.count("e")
 
-True_cal = int(count_t + count_r + count_u + count_e)
-love_cal = int(count_l + count_o + count_v + count_e)
-love_cal = str(love_cal)
-True_cal = str(True_cal)
+True_cal = str(int(count_t + count_r + count_u + count_e))
+love_cal = str(int(count_l + count_o + count_v + count_e))
 love_score = True_cal + love_cal
 love_score = int(love_score)
 
@@ -38,3 +26,4 @@ elif love_score > 39 and love_score < 51:
   print(f"Your score is {love_score}, you are alright together.")
 else:
  print(f"Your score is {love_score}.")
+
